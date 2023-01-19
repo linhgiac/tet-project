7402406ea6e717fa0766e08ed44a1af3d4bd4587
linhgiac <khanhly.hcmut@gmail.com>
2023-01-19 23:22:40 +0700

    client: setup routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<Home />}
+                    />
+                    <Route
+                        path="/login"
+                        element={<Login />}
+                    />
+                    <Route
+                        path="/register"
+                        element={<Register />}
+                    />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
